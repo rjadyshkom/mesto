6c184b6a1568327a889d6d1c8fafe96496b5b798
Sergey Mitrofanov <rjadysh.com@yandex.ru>
2021-03-24 00:09:40 +0300

    feat: логика JS Практика 5

--- blocks/root/root.js
+++ blocks/root/root.js
@@ -1,23 +1,90 @@
-let editButton = document.querySelector('.button_type_edit');
-let popup = document.querySelector('.popup');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let popupName = document.querySelector('.popup__name');
-let popupAbout = document.querySelector('.popup__about');
-let formElement = popup.querySelector('.popup__form');
-let closeButton = document.querySelector('.button_type_close');
+const editButton = document.querySelector('.button_type_edit');
+const addButton = document.querySelector('.button_type_add');
+const closeButton = document.querySelector('.button_type_close');
+const closeLightboxButton = document.querySelector('.lightbox_close');
+const popup = document.querySelector('.popup');
+const lightbox = document.querySelector('.lightbox');
+const popupTitle = document.querySelector('.popup__title');
+const popupName = document.querySelector('.popup__name');
+const popupAbout = document.querySelector('.popup__about');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const formElement = popup.querySelector('.popup__form');
+const elementsContainer = document.querySelector('.elements__inner');
+const initialCards = [
+    {
+        name: 'Франция',
+        link: './images/elements-france.jpg'
+    },
+    {
+        name: 'Исландия',
+        link: './images/elements-iceland.jpg'
+    },
+    {
+        name: 'Швейцария',
+        link: './images/elements-switzerland.jpg'
+    },
+    {
+        name: 'Великобритания',
+        link: './images/elements-united-kingdom.jpg'
+    },
+    {
+        name: 'США',
+        link: './images/elements-usa.jpg'
+    },
+    {
+        name: 'Новая Зеландия',
+        link: './images/elements-new-zealand.jpg'
+    }
+];
+
+
+initialCards.forEach(element => {
+    const cardTemplate = document.querySelector('#element-template').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+    cardElement.querySelector('.element__image').src = element.link;
+    cardElement.querySelector('.element__image').alt = element.name;
+    cardElement.querySelector('.element__title').textContent = element.name;
+
+    cardElement.querySelector('.button_type_trash').addEventListener('click', () => {
+        cardElement.remove();
+    });
+
+
+    cardElement.querySelector('.button_type_like').addEventListener('click', evt => {
+        evt.target.classList.toggle('button_type_like_active');
+    });
+
+    cardElement.querySelector('.element__image').addEventListener('click', () => {
+        openLightbox();
+        document.querySelector('.lightbox__image').src = element.link;
+        document.querySelector('.lightbox__caption').textContent = element.name;
+    })
+
+    elementsContainer.append(cardElement);
+});
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    popupTitle.textContent = 'Редактировать профиль';
     popupName.value = profileName.textContent;
     popupAbout.value = profileAbout.textContent;
+    formElement.setAttribute('data', 'editForm');
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened');
-    popupName.value = '';
-    popupAbout.value = '';
 }
+
+function openLightbox() {
+    lightbox.classList.add('lightbox_opened');
+}
+
+function closeLightbox() {
+    lightbox.classList.remove('lightbox_opened');
+}
+
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -26,6 +93,42 @@ function formSubmitHandler(evt) {
     closePopup();
 }
 
+function formSubmitAddCard(evt) {
+    evt.preventDefault();
+    const cardTemplate = document.querySelector('#element-template').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+    cardElement.querySelector('.element__image').src = popupAbout.value;
+    cardElement.querySelector('.element__title').textContent = popupName.value;
+    cardElement.querySelector('.button_type_trash').addEventListener('click', () => {
+        cardElement.remove();
+    });
+    cardElement.querySelector('.button_type_like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('button_type_like_active');
+    });
+    elementsContainer.prepend(cardElement);
+    closePopup();
+}
+
+
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+closeLightboxButton.addEventListener('click', closeLightbox);
+
+addButton.addEventListener('click', () => {
+    openPopup();
+    popupTitle.textContent = 'Новое место';
+    popupName.value = 'Название';
+    popupAbout.value = 'Ссылка на картинку';
+    formElement.setAttribute('data', 'addForm');
+});
+
+
+formElement.addEventListener('submit', e => {
+        const formDataAttribute = formElement.getAttribute('data')
+        if (formDataAttribute === 'editForm') {
+            return formSubmitHandler(e);
+        } else {
+            return formSubmitAddCard(e);
+        }
+    }
+);
